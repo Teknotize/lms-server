210895150a59f06e70caf53f858145746a70b044
Muhammad Ammad <me.ammad1786@gmail.com>
2024-10-01 20:04:16 +0500

    On Employee List view page - on hover profile pic -> scale 2 times

--- application/views/employee/view.php
+++ application/views/employee/view.php
@@ -2,19 +2,19 @@
 	<div class="tabs-custom">
 		<ul class="nav nav-tabs">
 			<?php
-			$this->db->where_not_in('id', array(1,6,7));
+			$this->db->where_not_in('id', array(1, 6, 7));
 
-			if(!is_superadmin_loggedin()){
+			if (!is_superadmin_loggedin()) {
 				$this->db->where('name !=', 'Directorate');
 			}
 			$roles = $this->db->get('roles')->result();
-			foreach ($roles as $role){
-			?> 	
-			<li class="<?php if ($role->id == $act_role) echo 'active'; ?>">
-				<a href="<?php echo base_url('employee/view/' . $role->id); ?>">
-					<i class="far fa-user-circle"></i> <?php echo $role->name?>
-				</a>
-			</li>
+			foreach ($roles as $role) {
+			?>
+				<li class="<?php if ($role->id == $act_role) echo 'active'; ?>">
+					<a href="<?php echo base_url('employee/view/' . $role->id); ?>">
+						<i class="far fa-user-circle"></i> <?php echo $role->name ?>
+					</a>
+				</li>
 			<?php } ?>
 		</ul>
 		<div class="tab-content">
@@ -32,49 +32,55 @@
 							<th><?php echo translate('department'); ?></th>
 							<th><?php echo translate('email'); ?></th>
 							<th><?php echo translate('mobile_no'); ?></th>
-						<?php
-						if (!is_superadmin_loggedin()){
-							$show_custom_fields = custom_form_table('employee', get_loggedin_branch_id());
-							if (count($show_custom_fields)) {
-								foreach ($show_custom_fields as $fields) {
+							<?php
+							if (!is_superadmin_loggedin()) {
+								$show_custom_fields = custom_form_table('employee', get_loggedin_branch_id());
+								if (count($show_custom_fields)) {
+									foreach ($show_custom_fields as $fields) {
 							?>
-							<th><?=$fields['field_label']?></th>
-						<?php } } } ?>
+										<th><?= $fields['field_label'] ?></th>
+							<?php }
+								}
+							} ?>
 							<th><?php echo translate('action'); ?></th>
 						</tr>
 					</thead>
 					<tbody>
-						<?php $i = 1; foreach ($stafflist as $row): ?>
-						<tr>
-							<td><?php echo $i++; ?></td>
-							<td class="center">
-								<img src="<?php echo get_image_url('staff', $row->photo); ?>" height="50" />
-							</td>
-							<td><?php echo get_type_name_by_id('branch', $row->branch_id);?></td>
-							<td><?php echo $row->staff_id; ?></td>
-							<td><?php echo $row->name; ?></td>
-							<td><?php echo $row->designation_name; ?></td>
-							<td><?php echo $row->department_name; ?></td>
-							<td><?php echo $row->email; ?></td>
-							<td><?php echo $row->mobileno; ?></td>
-						<?php
-						if (!is_superadmin_loggedin()){
-							if (count($show_custom_fields)) {
-								foreach ($show_custom_fields as $fields) {
-							?>
-							<td><?php echo get_table_custom_field_value($fields['id'], $row->id);?></td>
-						<?php } } } ?>
-							<td class="min-w-c">
-							<?php if (get_permission('employee', 'is_edit')): ?>
-								<a href="<?php echo base_url('employee/profile/'.$row->id); ?>" class="btn btn-circle btn-default icon" data-toggle="tooltip" 
-								data-original-title="<?=translate('profile')?>">
-									<i class="far fa-arrow-alt-circle-right"></i>
-								</a>
-							<?php endif; if (get_permission('employee', 'is_delete')): ?>
-								<?php echo btn_delete('employee/delete/' . $row->id); ?>
-							<?php endif; ?>
-							</td>
-						</tr>
+						<?php $i = 1;
+						foreach ($stafflist as $row): ?>
+							<tr>
+								<td><?php echo $i++; ?></td>
+								<td class="center zoom">
+									<img src="<?php echo get_image_url('staff', $row->photo); ?>" height="50" />
+								</td>
+								<td><?php echo get_type_name_by_id('branch', $row->branch_id); ?></td>
+								<td><?php echo $row->staff_id; ?></td>
+								<td><?php echo $row->name; ?></td>
+								<td><?php echo $row->designation_name; ?></td>
+								<td><?php echo $row->department_name; ?></td>
+								<td><?php echo $row->email; ?></td>
+								<td><?php echo $row->mobileno; ?></td>
+								<?php
+								if (!is_superadmin_loggedin()) {
+									if (count($show_custom_fields)) {
+										foreach ($show_custom_fields as $fields) {
+								?>
+											<td><?php echo get_table_custom_field_value($fields['id'], $row->id); ?></td>
+								<?php }
+									}
+								} ?>
+								<td class="min-w-c">
+									<?php if (get_permission('employee', 'is_edit')): ?>
+										<a href="<?php echo base_url('employee/profile/' . $row->id); ?>" class="btn btn-circle btn-default icon" data-toggle="tooltip"
+											data-original-title="<?= translate('profile') ?>">
+											<i class="far fa-arrow-alt-circle-right"></i>
+										</a>
+									<?php endif;
+									if (get_permission('employee', 'is_delete')): ?>
+										<?php echo btn_delete('employee/delete/' . $row->id); ?>
+									<?php endif; ?>
+								</td>
+							</tr>
 						<?php endforeach; ?>
 					</tbody>
 				</table>
